75d6e06e9538e72207f982c4fc641c19b90b0b36
Anushree Adyanthaya <anushreeadyanthaya@gmail.com>
2026-08-16 13:49:15 +0530

    Prepare frontend for production deployment

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,3 +1,5 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://healtheon-backend.onrender.com";
 
 export default API_URL;
